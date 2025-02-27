91b6f7aa1a5e0184521c1eb03aa25f034a8e3c35
Thomas Maniero <thomas@maniero.info>
2025-02-27 10:57:01 +0100

    updated cell index calculation to account for the table's scale value

--- plugins/gameobjects/container/gridtable/methods/PointToCell.js
+++ plugins/gameobjects/container/gridtable/methods/PointToCell.js
@@ -1,12 +1,12 @@
 var PointToCellIndex = function (x, y) {
     y -= (this.y + this.topLeftY);
     x -= (this.x + this.topLeftX);
-    var offsetTableOY = this.tableOY - ((this.scrollMode === 0) ? y : x);
-    var offsetTableOX = this.tableOX - ((this.scrollMode === 0) ? x : y);
+    var offsetTableOY = this.tableOY * this.scaleY - ((this.scrollMode === 0) ? y : x);
+    var offsetTableOX = this.tableOX * this.scaleX - ((this.scrollMode === 0) ? x : y);
 
     var table = this.table;
-    var rowIdx = table.heightToRowIndex(-offsetTableOY, 0);
-    var colIdx = table.widthToColIndex(-offsetTableOX);
+    var rowIdx = table.heightToRowIndex(-offsetTableOY / this.scaleY, 0);
+    var colIdx = table.widthToColIndex(-offsetTableOX / this.scaleX);
     var cellIdx = table.colRowToCellIndex(colIdx, rowIdx);
     if (cellIdx === null) {
         return null;
